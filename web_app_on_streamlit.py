bc6a2e7bfca1180fc5268b798138c6f099257a5d
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-03-31 11:09:19 +0300

    running python app on internet

---
+++ web_app_on_streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st 
+
+st.title("1st StreaLit app")
+st.subheader("A lot of stuff to learn")
+st.write("""No worries!
+We'll get there sooner or later!
+Enjoy!
+""")
+
+# to run this app add in terminal this command - streamlit run web_app_on_streamlit.py 
+
