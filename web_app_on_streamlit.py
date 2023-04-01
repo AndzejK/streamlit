8e08cee3d84df3d447d3f95dae805a3123989258
Andzej Krupoves <andrejkrupovec@gmail.com>
2023-04-01 09:15:09 +0300

    https://share.streamlit.io

--- web_app_on_streamlit.py
+++ web_app_on_streamlit.py
@@ -4,8 +4,10 @@ st.title("1st StreaLit app")
 st.subheader("A lot of stuff to learn")
 st.write("""No worries!
 We'll get there sooner or later!
-Enjoy!
+""")
+st.write("""P.S.Enjoy!
 """)
 
-# to run this app add in terminal this command - streamlit run web_app_on_streamlit.py 
+# to run this app add in terminal this command - streamlit run web_app_on_streamlit.py
+# https://share.streamlit.io/ hoste this app on streamlit 
 
